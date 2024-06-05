7c6fc9822e28f11ccc791d33b67540fdd583ac12
su6net <su6net@gmail.com>
2024-06-05 22:17:31 +0900

    Config: 초기 프로젝트 설정 및 의존성 추가

---
+++ src/main/java/com/growup/pms/GrowUpPmsApplication.java
@@ -0,0 +1,13 @@
+package com.growup.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowUpPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowUpPmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/growup/pms/GrowUpPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growup.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowUpPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
