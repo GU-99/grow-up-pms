b85a22206816e70ab1f182962b5183b4dc3dec6e
su6net <su6net@gmail.com>
2024-06-23 00:08:01 +0900

    Refactor: BusinessException에서 ErrorResponse 필드 제거
    BusinessException 클래스에서 ErrorResponse 필드를 제거하였습니다.
    ErrorResponse는 ErrorCode로부터 생성할 수 있으므로 예외 클래스에 직접 포함할 필요가 없습니다.
    이로 인해 클래스의 단순성과 책임이 분리되어 코드의 명확성을 높일 수 있습니다.

--- src/main/java/com/growup/pms/common/exception/exceptions/BusinessException.java
+++ src/main/java/com/growup/pms/common/exception/exceptions/BusinessException.java
@@ -1,17 +1,14 @@
 package com.growup.pms.common.exception.exceptions;
 
 import com.growup.pms.common.exception.code.ErrorCode;
-import com.growup.pms.common.exception.dto.ErrorResponse;
 import lombok.Getter;
 
 @Getter
 public class BusinessException extends RuntimeException {
     private final ErrorCode errorCode;
-    private final transient ErrorResponse errorResponse; // 일반적으로 예외 처리를 위해 사용되므로 직렬화 로직을 구현할 필요가 없음
 
     public BusinessException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
-        this.errorResponse = ErrorResponse.of(errorCode);
     }
 }
