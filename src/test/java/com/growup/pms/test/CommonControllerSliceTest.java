b992b01e5266c593acf5cde2f22ae80de1095f45
yachimiya <hachimiya@pm.me>
2024-07-01 19:55:55 +0900

    Feat: 테스트 격리를 위해 테스트가 끝난 후 모킹된 객체를 초기화함

--- src/test/java/com/growup/pms/test/CommonControllerSliceTest.java
+++ src/test/java/com/growup/pms/test/CommonControllerSliceTest.java
@@ -3,12 +3,16 @@ package com.growup.pms.test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.growup.pms.common.security.jwt.JwtTokenProvider;
 import com.growup.pms.test.annotation.AutoServiceMockBeans;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Service;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +27,9 @@ public abstract class CommonControllerSliceTest {
     @Autowired
     protected ObjectMapper objectMapper;
 
+    @Autowired
+    private ConfigurableListableBeanFactory beanFactory;
+
     @MockBean
     protected JwtTokenProvider jwtTokenProvider;
 
@@ -30,5 +37,16 @@ public abstract class CommonControllerSliceTest {
     void setUpEach(WebApplicationContext context) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .build();
+    }
+
+    @AfterEach
+    void resetMocks() {
+        String[] mockBeanNames = beanFactory.getBeanNamesForAnnotation(Service.class);
+        for (String beanName : mockBeanNames) {
+            Object bean = beanFactory.getBean(beanName);
+            if (Mockito.mockingDetails(bean).isMock()) {
+                Mockito.reset(bean);
+            }
+        }
     }
 }
