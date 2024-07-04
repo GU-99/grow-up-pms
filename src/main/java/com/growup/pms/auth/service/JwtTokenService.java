bddc3a496fe61e46aca1ed5a14e69906a8b22388
yachimiya <hachimiya@pm.me>
2024-07-04 10:24:48 +0900

    Refactor: 리프레시 토큰을 통해 유저 정보를 가져올 수 있으므로 불필요한 필드를 제거함

--- src/main/java/com/growup/pms/auth/service/JwtTokenService.java
+++ src/main/java/com/growup/pms/auth/service/JwtTokenService.java
@@ -7,6 +7,7 @@ import com.growup.pms.common.security.jwt.JwtTokenProvider;
 import com.growup.pms.common.security.jwt.dto.TokenDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -14,15 +15,16 @@ public class JwtTokenService {
     private final JwtTokenProvider tokenProvider;
     private final RefreshTokenService refreshTokenService;
 
-    public TokenDto refreshJwtTokens(SecurityUser user, String oldRefreshToken) {
+    public TokenDto refreshJwtTokens(String oldRefreshToken) {
         validateRefreshToken(oldRefreshToken);
-        TokenDto newToken = tokenProvider.generateToken(user);
-        refreshTokenService.renewRefreshToken(user.getId(), newToken.getRefreshToken());
+        SecurityUser currentUser = (SecurityUser) tokenProvider.getAuthentication(oldRefreshToken).getPrincipal();
+        TokenDto newToken = tokenProvider.generateToken(currentUser);
+        refreshTokenService.renewRefreshToken(currentUser.getId(), newToken.getRefreshToken());
         return newToken;
     }
 
     private void validateRefreshToken(String token) {
-        if (!refreshTokenService.validateToken(token)) {
+        if (!StringUtils.hasText(token) || !refreshTokenService.validateToken(token)) {
             throw new AuthenticationException(ErrorCode.INVALID_REFRESH_TOKEN_ERROR);
         }
     }
