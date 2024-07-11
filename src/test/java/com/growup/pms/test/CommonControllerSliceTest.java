a253393da9f835de4fb42c20e3bb40bea2958235
yachimiya <hachimiya@pm.me>
2024-07-11 21:46:46 +0900

    Test: 테스트에서 새로 추가한 JSON 관련 모듈을 이용할 수 있도록 함

--- src/test/java/com/growup/pms/test/CommonControllerSliceTest.java
+++ src/test/java/com/growup/pms/test/CommonControllerSliceTest.java
@@ -1,6 +1,7 @@
 package com.growup.pms.test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.growup.pms.common.config.ObjectMapperConfig;
 import com.growup.pms.common.security.jwt.JwtTokenProvider;
 import com.growup.pms.test.annotation.AutoServiceMockBeans;
 import org.junit.jupiter.api.AfterEach;
@@ -12,12 +13,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Service;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
 
+@Import(ObjectMapperConfig.class)
 @AutoServiceMockBeans(basePackage = "com.growup.pms")
 @WebMvcTest(includeFilters = @Filter(type = FilterType.ANNOTATION, classes = RestController.class))
 public abstract class CommonControllerSliceTest {
