23368069c30353b4685a61c76b0a7e4d9db34f80
longBright <74171272+longBright@users.noreply.github.com>
2024-07-17 22:32:57 +0900

    Config: #74 통합 테스트를 위한 공통 클래스 작성 (#75)
    * Config: 통합 테스트를 위한 공통 클래스 작성
    
    * Comment: 코드 리뷰에 따른 불필요한 주석 제거

---
+++ src/test/java/com/growup/pms/IntegrationTestSupport.java
@@ -0,0 +1,9 @@
+package com.growup.pms;
+
+import jakarta.transaction.Transactional;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+public abstract class IntegrationTestSupport {
+}
