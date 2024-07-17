c04649ce8e0bbe49f80c4f2b7c8c03bc930bac6d
longBright <longbright0804@gmail.com>
2024-07-17 22:24:17 +0900

    Comment: 코드 리뷰에 따른 불필요한 주석 제거

--- src/test/java/com/growup/pms/IntegrationTestSupport.java
+++ src/test/java/com/growup/pms/IntegrationTestSupport.java
@@ -3,11 +3,6 @@ package com.growup.pms;
 import jakarta.transaction.Transactional;
 import org.springframework.boot.test.context.SpringBootTest;
 
-/**
- * 통합 테스트를 위한 공통 클래스
- *
- * @author 최영환
- */
 @SpringBootTest
 @Transactional
 public abstract class IntegrationTestSupport {
