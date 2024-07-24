a127b879d048d975897eb5a8599fde4911d22afb
longbright <longbright0804@gmail.com>
2024-07-24 22:41:53 +0900

    Chore: 프로젝트 상태 테이블의 수정사항을 매핑되는 엔티티인 Status 에 반영

--- src/main/java/com/growup/pms/status/domain/Status.java
+++ src/main/java/com/growup/pms/status/domain/Status.java
@@ -1,10 +1,15 @@
 package com.growup.pms.status.domain;
 
+import com.growup.pms.project.domain.Project;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,22 +17,32 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(name = "project_status")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Status {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "status_id")
+    @Column(name = "project_status_id")
     private Long id;
 
-    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false, updatable = false)
+    private Project project;
+
+    @Column(nullable = false, length = 32)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 6)
     private String color;
 
+    @Column(nullable = false)
+    private Short sortOrder;
+
     @Builder
-    public Status(String name, String color) {
+    public Status(Project project, String name, String color, Short sortOrder) {
+        this.project = project;
         this.name = name;
         this.color = color;
+        this.sortOrder = sortOrder;
     }
 }
