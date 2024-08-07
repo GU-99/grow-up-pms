222d3cc8354f4c3c0868fccecd10d65a76de8315
longbright <longbright0804@gmail.com>
2024-08-07 17:29:43 +0900

    Feat: 프로젝트 일정 레포지토리 생성 및 JPA 주입

---
+++ src/main/java/com/growup/pms/task/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.growup.pms.task.repository;
+
+import com.growup.pms.task.domain.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
