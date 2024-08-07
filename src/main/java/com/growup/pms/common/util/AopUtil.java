5666f1d48caabf0faa16f5b60c25d83412f026b5
yachimiya <hachimiya@pm.me>
2024-08-07 17:54:14 +0900

    Feat: 조인 포인트에서 원하는 조건의 파라미터를 찾는 헬퍼 메서드를 추가함

---
+++ src/main/java/com/growup/pms/common/util/AopUtil.java
@@ -0,0 +1,37 @@
+package com.growup.pms.common.util;
+
+import java.lang.annotation.Annotation;
+import java.util.List;
+import java.util.stream.IntStream;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+
+public interface AopUtil {
+    static List<Parameter> extractAnnotatedParameters(JoinPoint joinPoint, Class<? extends Annotation> clazz) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        java.lang.reflect.Parameter[] parameters = signature.getMethod().getParameters();
+        Object[] args = joinPoint.getArgs();
+
+        return IntStream.range(0, parameters.length)
+                .filter(i -> parameters[i].isAnnotationPresent(clazz))
+                .mapToObj(i -> new Parameter(parameters[i].getName(), args[i], parameters[i].getType()))
+                .toList();
+    }
+
+    static List<Parameter> filterAnnotatedParametersByType(JoinPoint joinPoint, Class<? extends Annotation> annotationClass, Class<?> parameterType) {
+        return extractAnnotatedParameters(joinPoint, annotationClass).stream()
+                .filter(param -> parameterType.isAssignableFrom(param.type))
+                .toList();
+    }
+
+    @SuppressWarnings("unchecked")
+    static <T> T findFirstAnnotatedParameterOfType(JoinPoint joinPoint, Class<? extends Annotation> annotationClass, Class<T> parameterType) {
+        return filterAnnotatedParametersByType(joinPoint, annotationClass, parameterType).stream()
+                .findFirst()
+                .map(param -> (T) param.value)
+                .orElseThrow(() -> new IllegalStateException("'%s' 애노테이션이 붙은 '%s' 타입의 파라미터를 찾을 수 없습니다."
+                        .formatted(annotationClass.getSimpleName(), parameterType.getSimpleName())));
+    }
+
+    record Parameter(String name, Object value, Class<?> type) { }
+}
