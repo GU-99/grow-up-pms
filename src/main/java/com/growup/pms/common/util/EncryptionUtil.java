2f07a223b9419e22923ce35afe445036c44cc942
longbright <longbright0804@gmail.com>
2024-08-08 23:03:29 +0900

    Feat: 암복호화 시 URL 인/디코더 사용하도록 수정 및 빈 등록

--- src/main/java/com/growup/pms/common/util/EncryptionUtil.java
+++ src/main/java/com/growup/pms/common/util/EncryptionUtil.java
@@ -3,44 +3,59 @@ package com.growup.pms.common.util;
 import com.growup.pms.common.exception.code.ErrorCode;
 import com.growup.pms.common.exception.exceptions.DecryptionException;
 import com.growup.pms.common.exception.exceptions.EncryptionException;
+import jakarta.annotation.PostConstruct;
 import java.util.Base64;
 import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Slf4j
+@Component
 public class EncryptionUtil {
 
     private static final String ALGORITHM = "AES";
-    private static byte[] key;
+    private static final String PADDING = "/ECB/PKCS5Padding";
 
-    @Value("${encryption.key}")
-    public void generateKey(String key) {
-        if (key == null || key.length() != 16) {
-            throw new IllegalArgumentException("잘못된 키 입니다.");
-        }
-        EncryptionUtil.key = key.getBytes();
+    @Value("${encryption.secret-key}")
+    private String secretKey;
+
+    private static String SECRET;
+
+    @PostConstruct
+    public void init() {
+        SECRET = secretKey;
+    }
+
+    private static SecretKey generateKey() {
+        byte[] keyBytes = SECRET.getBytes();
+        return new SecretKeySpec(keyBytes, ALGORITHM);
     }
 
     public static String encrypt(String plainText) {
         try {
-            SecretKeySpec secretKey = new SecretKeySpec(key, ALGORITHM);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            Cipher cipher = Cipher.getInstance(ALGORITHM + PADDING);
+            cipher.init(Cipher.ENCRYPT_MODE, generateKey());
             byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
-            return Base64.getEncoder().encodeToString(encryptedBytes);
+            return Base64.getUrlEncoder().encodeToString(encryptedBytes);
         } catch (Exception e) {
+            log.error("Encryption error: ", e);
             throw new EncryptionException(ErrorCode.ENCRYPTION_ERROR);
         }
     }
 
-    public static String decrypt(String encryptedText) {
+    public static Long decrypt(String encryptedText) {
         try {
-            SecretKeySpec secretKey = new SecretKeySpec(key, ALGORITHM);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
-            return new String(decryptedBytes);
+            Cipher cipher = Cipher.getInstance(ALGORITHM + PADDING);
+            cipher.init(Cipher.DECRYPT_MODE, generateKey());
+            byte[] encryptedBytes = Base64.getUrlDecoder().decode(encryptedText);
+            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+            String decryptedString = new String(decryptedBytes);
+            return Long.parseLong(decryptedString);
         } catch (Exception e) {
+            log.error("Decryption error: ", e);
             throw new DecryptionException(ErrorCode.DECRYPTION_ERROR);
         }
     }
