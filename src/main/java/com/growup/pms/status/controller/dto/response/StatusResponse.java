3f431b639dfa1f49f6624759fbe60ea24bdd19b9
longbright <longbright0804@gmail.com>
2024-08-13 01:12:37 +0900

    Feat: 프로젝트 상태 등록 API 구현

--- src/main/java/com/growup/pms/status/controller/dto/response/StatusResponse.java
+++ src/main/java/com/growup/pms/status/controller/dto/response/StatusResponse.java
@@ -1,7 +1,17 @@
 package com.growup.pms.status.controller.dto.response;
 
+import com.growup.pms.status.domain.Status;
 import lombok.Builder;
 
 @Builder
 public record StatusResponse(Long statusId, Long projectId, String name, String colorCode, Short sortOrder) {
+    public static StatusResponse of(Status status) {
+        return StatusResponse.builder()
+                .statusId(status.getId())
+                .projectId(status.getProject().getId())
+                .name(status.getName())
+                .colorCode(status.getColorCode())
+                .sortOrder(status.getSortOrder())
+                .build();
+    }
 }
--- src/main/java/com/growup/pms/status/service/StatusService.java
+++ src/main/java/com/growup/pms/status/service/StatusService.java
@@ -1,7 +1,12 @@
 package com.growup.pms.status.service;
 
+import com.growup.pms.common.exception.code.ErrorCode;
+import com.growup.pms.common.exception.exceptions.EntityNotFoundException;
+import com.growup.pms.project.domain.Project;
+import com.growup.pms.project.repository.ProjectRepository;
 import com.growup.pms.status.controller.dto.response.PageResponse;
 import com.growup.pms.status.controller.dto.response.StatusResponse;
+import com.growup.pms.status.domain.Status;
 import com.growup.pms.status.repository.StatusRepository;
 import com.growup.pms.status.service.dto.StatusCreateCommand;
 import com.growup.pms.status.service.dto.StatusEditCommand;
@@ -18,10 +23,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class StatusService {
 
     private final StatusRepository statusRepository;
+    private final ProjectRepository projectRepository;
 
     @Transactional
-    public StatusResponse createStatus(StatusCreateCommand dto) {
-        return null;
+    public StatusResponse createStatus(StatusCreateCommand command) {
+        Project project = getProject(command);
+        Status savedStatus = statusRepository.save(command.toEntity(project));
+
+        return StatusResponse.of(savedStatus);
     }
 
     public PageResponse<List<StatusResponse>> getStatuses(Long projectId) {
@@ -35,5 +44,10 @@ public class StatusService {
     }
 
     public void deleteStatus(Long statusId) {
+    }
+
+    private Project getProject(StatusCreateCommand command) {
+        return projectRepository.findById(command.projectId())
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.PROJECT_NOT_FOUND));
     }
 }
