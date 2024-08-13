822e9b20215d948b3d37febf1b47e5b3bbd11574
longbright <longbright0804@gmail.com>
2024-08-13 23:19:00 +0900

    Feat: 식별키를 사용한 단건 조회 default 메소드 작성

--- src/main/java/com/growup/pms/project/repository/ProjectRepository.java
+++ src/main/java/com/growup/pms/project/repository/ProjectRepository.java
@@ -1,8 +1,13 @@
 package com.growup.pms.project.repository;
 
+import com.growup.pms.common.exception.code.ErrorCode;
+import com.growup.pms.common.exception.exceptions.EntityNotFoundException;
 import com.growup.pms.project.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
+    default Project findByIdOrThrow(Long id) {
+        return findById(id).orElseThrow(() -> new EntityNotFoundException(ErrorCode.PROJECT_NOT_FOUND));
+    }
 }
