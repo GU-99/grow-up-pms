8ee99ba66232569ef9c901289b7995a987435aed
longbright <longbright0804@gmail.com>
2024-08-18 02:19:49 +0900

    Feat: 일정 조회 레포지토리 생성

--- src/main/java/com/growup/pms/task/repository/TaskRepository.java
+++ src/main/java/com/growup/pms/task/repository/TaskRepository.java
@@ -3,5 +3,5 @@ package com.growup.pms.task.repository;
 import com.growup.pms.task.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, TaskQueryRepository {
 }
