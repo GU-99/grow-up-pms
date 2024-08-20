9e5bd71409741b3027371cd4228431a980ae6765
yachimiya <hachimiya@pm.me>
2024-08-20 16:54:58 +0900

    Chore: 기본 에러 페이지를 비활성화함

--- src/main/java/com/growup/pms/GrowUpPmsApplication.java
+++ src/main/java/com/growup/pms/GrowUpPmsApplication.java
@@ -2,8 +2,9 @@ package com.growup.pms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class GrowUpPmsApplication {
 
     public static void main(String[] args) {
