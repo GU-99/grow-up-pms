1f69b2a3f8909781affe054823493e86a8a37617
yachimiya <hachimiya@pm.me>
2024-08-20 18:29:35 +0900

    Test: 유닛 테스트와 문서 테스트를 작성함

--- src/test/java/com/growup/pms/team/service/TeamUserServiceTest.java
+++ src/test/java/com/growup/pms/team/service/TeamUserServiceTest.java
@@ -2,12 +2,14 @@ package com.growup.pms.team.service;
 
 import static com.growup.pms.test.fixture.role.RoleTestBuilder.역할은;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import com.growup.pms.common.exception.exceptions.AuthorizationException;
 import com.growup.pms.common.exception.exceptions.EntityNotFoundException;
+import com.growup.pms.common.exception.exceptions.InvalidInputException;
 import com.growup.pms.role.domain.Role;
 import com.growup.pms.role.domain.TeamRole;
 import com.growup.pms.team.domain.TeamUserId;
@@ -17,6 +19,8 @@ import java.util.Optional;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -75,6 +79,38 @@ class TeamUserServiceTest {
             assertThatCode(() -> teamUserService.kickMember(팀_ID, 추방할_팀원_ID))
                     .isInstanceOf(AuthorizationException.class)
                     .hasMessage("접근 권한이 없습니다.");
+        }
+    }
+
+    @Nested
+    class 팀원_역할_변경_시 {
+        @ParameterizedTest
+        @EnumSource(value = TeamRole.class, names = {"LEADER", "MATE"})
+        void 성공한다(TeamRole 새로운_역할) {
+            // given
+            Long 팀_ID = 1L;
+            Long 역할_변경할_팀원_ID = 2L;
+            Long 변경된_레코드_수 = 1L;
+            String 새로운_역할명 = 새로운_역할.getRoleName();
+
+            when(teamUserRepository.updateRoleForTeamUser(팀_ID, 역할_변경할_팀원_ID, 새로운_역할명)).thenReturn(변경된_레코드_수);
+
+            // when & then
+            assertThatCode(() -> teamUserService.changeRole(팀_ID, 역할_변경할_팀원_ID, 새로운_역할명))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void 팀원을_관리자로_지정하려는_경우_예외가_발생한다() {
+            // given
+            Long 팀_ID = 1L;
+            Long 역할_변경할_팀원_ID = 2L;
+            String 변경할_역할명 = TeamRole.HEAD.getRoleName();
+
+            // when & then
+            assertThatThrownBy(() -> teamUserService.changeRole(팀_ID, 역할_변경할_팀원_ID, 변경할_역할명))
+                    .isInstanceOf(InvalidInputException.class)
+                    .hasMessage("허용되지 않은 역할 지정입니다.");
         }
     }
 }
