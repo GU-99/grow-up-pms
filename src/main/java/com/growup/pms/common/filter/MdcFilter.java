80567ea30f613a12e98b1bd53f707b876784ba19
yachimiya <hachimiya@pm.me>
2024-08-22 00:09:43 +0900

    Chore: 가독성을 위해 좀 더 짧은 ID를 사용하도록 수정함

--- src/main/java/com/growup/pms/common/filter/MdcFilter.java
+++ src/main/java/com/growup/pms/common/filter/MdcFilter.java
@@ -5,18 +5,19 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.UUID;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.MDC;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class MdcFilter extends OncePerRequestFilter {
-    public static final String TRACKING_ID = "trackingId";
+    public static final String TRACKING_ID = "traceId";
+    public static final int LENGTH_TRACKING_ID = 11;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         try {
-            MDC.put(TRACKING_ID, UUID.randomUUID().toString());
+            MDC.put(TRACKING_ID, RandomStringUtils.randomAlphanumeric(LENGTH_TRACKING_ID).toUpperCase());
             filterChain.doFilter(request, response);
         } finally {
             MDC.clear();
