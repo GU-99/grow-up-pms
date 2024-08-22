c31800bdaa9f3485c56b047d89f8bd93a71a7010
yachimiya <hachimiya@pm.me>
2024-08-22 19:13:43 +0900

    Chore: 로그 ID 충돌을 피하기 위해서 추적 ID를 UUID로 대체함

--- src/main/java/com/growup/pms/common/filter/MdcFilter.java
+++ src/main/java/com/growup/pms/common/filter/MdcFilter.java
@@ -5,22 +5,25 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.apache.commons.lang3.RandomStringUtils;
+import java.util.UUID;
 import org.slf4j.MDC;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class MdcFilter extends OncePerRequestFilter {
     public static final String TRACKING_ID = "traceId";
-    public static final int LENGTH_TRACKING_ID = 11;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         try {
-            MDC.put(TRACKING_ID, RandomStringUtils.randomAlphanumeric(LENGTH_TRACKING_ID).toUpperCase());
+            MDC.put(TRACKING_ID, generateTraceId());
             filterChain.doFilter(request, response);
         } finally {
             MDC.clear();
         }
+    }
+
+    private String generateTraceId() {
+        return UUID.randomUUID().toString().replace("-", "").toUpperCase();
     }
 }
