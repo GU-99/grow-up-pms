d2244a5c8ac7a34731ec206345e442bd190daee1
yachimiya <hachimiya@pm.me>
2024-08-27 17:14:20 +0900

    Feat: 프록시에서 내부 호출 문제를 해결하기 위한 별도의 핸들러 정의

---
+++ src/main/java/com/growup/pms/auth/service/TransactionHandler.java
@@ -0,0 +1,20 @@
+package com.growup.pms.auth.service;
+
+import java.util.function.Supplier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class TransactionHandler {
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public <T> T runInTransaction(Supplier<T> supplier) {
+        return supplier.get();
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public <T> T runInNewTransaction(Supplier<T> supplier) {
+        return supplier.get();
+    }
+}
