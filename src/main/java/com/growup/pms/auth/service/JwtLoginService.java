e8ad512668dd9024660592e1244447a608ccc0b7
yachimiya <hachimiya@pm.me>
2024-08-30 01:05:32 +0900

    Fix: 인증 실패 시 스프링 예외를 반환하는 문제 수정함

--- src/main/java/com/growup/pms/auth/service/JwtLoginService.java
+++ src/main/java/com/growup/pms/auth/service/JwtLoginService.java
@@ -2,12 +2,15 @@ package com.growup.pms.auth.service;
 
 import com.growup.pms.auth.controller.dto.SecurityUser;
 import com.growup.pms.auth.service.dto.LoginCommand;
+import com.growup.pms.common.exception.code.ErrorCode;
+import com.growup.pms.common.exception.exceptions.BusinessException;
 import com.growup.pms.common.security.jwt.JwtTokenProvider;
 import com.growup.pms.common.security.jwt.dto.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,11 +24,19 @@ public class JwtLoginService {
     @Transactional
     public TokenResponse authenticateUser(LoginCommand command) {
         var authenticationToken = new UsernamePasswordAuthenticationToken(command.username(), command.password());
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        Authentication authentication = authenticate(authenticationToken);
         SecurityUser principal = (SecurityUser) authentication.getPrincipal();
         TokenResponse newToken = tokenProvider.generateToken(principal);
 
         redisRefreshTokenService.save(principal.getId(), newToken.refreshToken());
         return newToken;
+    }
+
+    private Authentication authenticate(UsernamePasswordAuthenticationToken authenticationToken) {
+        try {
+            return authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        } catch (AuthenticationException ex) {
+            throw new BusinessException(ErrorCode.AUTHENTICATION_FAILED);
+        }
     }
 }
