9fa9f67cd4713907a536b58de4741f31c3c04261
yachimiya <hachimiya@pm.me>
2024-09-01 18:25:30 +0900

    Fix: 누락된 애노테이션을 추가함

--- src/main/java/com/growup/pms/user/controller/dto/request/RecoverPasswordRequest.java
+++ src/main/java/com/growup/pms/user/controller/dto/request/RecoverPasswordRequest.java
@@ -4,18 +4,22 @@ import static com.growup.pms.common.constant.RegexConstants.USERNAME_PATTERN;
 
 import com.growup.pms.user.service.dto.RecoverPasswordCommand;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record RecoverPasswordRequest(
+        @NotNull
         @Email
         String email,
 
+        @NotNull
         @Pattern(regexp = USERNAME_PATTERN)
         String username,
 
+        @NotNull
         @Size(min = 6, max = 6)
         String verificationCode
 ) {
