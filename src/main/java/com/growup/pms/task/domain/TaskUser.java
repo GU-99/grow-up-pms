05158602a3b828987df75f3e7ba220783a8c941b
longbright <longbright0804@gmail.com>
2024-09-02 23:14:50 +0900

    Feat: `TaskUser` 엔티티 추가

---
+++ src/main/java/com/growup/pms/task/domain/TaskUser.java
@@ -0,0 +1,42 @@
+package com.growup.pms.task.domain;
+
+import com.growup.pms.common.BaseEntity;
+import com.growup.pms.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Table(name = "task_users")
+@SQLDelete(sql = "UPDATE task_users SET is_deleted = true WHERE id = ?")
+@SQLRestriction(("is_deleted = false"))
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TaskUser extends BaseEntity {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "fk_task_users_user"), nullable = false)
+    private User user;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", foreignKey = @ForeignKey(name = "fk_task_users_task"), nullable = false)
+    private Task task;
+
+    @Builder
+    public TaskUser(User user, Task task) {
+        this.user = user;
+        this.task = task;
+    }
+}
