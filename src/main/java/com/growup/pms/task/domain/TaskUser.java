ce6e8000a053ba11d35e3e72bd5e82aef0aa2c1b
longbright <longbright0804@gmail.com>
2024-09-03 00:33:42 +0900

    Feat: `TaskUser` 엔티티 Id 클래스 추가

--- src/main/java/com/growup/pms/task/domain/TaskUser.java
+++ src/main/java/com/growup/pms/task/domain/TaskUser.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -18,6 +19,7 @@ import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
+@IdClass(TaskUserId.class)
 @Table(name = "task_users")
 @SQLDelete(sql = "UPDATE task_users SET is_deleted = true WHERE id = ?")
 @SQLRestriction(("is_deleted = false"))
