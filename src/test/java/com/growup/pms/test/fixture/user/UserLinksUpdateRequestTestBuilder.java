5785bc66cba4c4141fa95707533da8dc0c56d444
wlshooo <kswlsgh@naver.com>
2024-09-05 19:32:25 +0900

    Test: 유저 정보 부분 변경에 대한 테스트와 링크 변경 테스트 추가

---
+++ src/test/java/com/growup/pms/test/fixture/user/UserLinksUpdateRequestTestBuilder.java
@@ -0,0 +1,31 @@
+package com.growup.pms.test.fixture.user;
+
+import com.growup.pms.user.controller.dto.request.UserLinksUpdateRequest;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+@SuppressWarnings("NonAsciiCharacters")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserLinksUpdateRequestTestBuilder {
+
+    private List<String> links = List.of("http://github.com", "http://blog.example.com", "http://GU-99.com");
+
+    public static UserLinksUpdateRequestTestBuilder 사용자_링크_변경_요청은() {
+        return new UserLinksUpdateRequestTestBuilder();
+    }
+
+    public UserLinksUpdateRequestTestBuilder 링크가(List<String> 링크) {
+        this.links = 링크;
+        return this;
+    }
+
+    public UserLinksUpdateRequest 이다() {
+        return UserLinksUpdateRequest.builder()
+                .links(JsonNullable.of(links))
+                .build();
+    }
+}
--- src/test/java/com/growup/pms/test/fixture/user/UserUpdateRequestTestBuilder.java
+++ src/test/java/com/growup/pms/test/fixture/user/UserUpdateRequestTestBuilder.java
@@ -1,19 +1,18 @@
 package com.growup.pms.test.fixture.user;
 
 import com.growup.pms.user.controller.dto.request.UserUpdateRequest;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.openapitools.jackson.nullable.JsonNullable;
 
 @SuppressWarnings("NonAsciiCharacters")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserUpdateRequestTestBuilder {
     private String nickname = "growUp";
-    private String bio = "안녕하세요 저는 grow up입니다. 잘부탁드려요~";
-    private String imageUrl = "http://example.com/profile.png";
-    private List<String> links = List.of("http://github.com", "http://blog.example.com");
+    private String bio;
+    private String profileImageUrl;
 
     public static UserUpdateRequestTestBuilder 사용자_정보_변경_요청은() {
         return new UserUpdateRequestTestBuilder();
@@ -30,21 +29,15 @@ public class UserUpdateRequestTestBuilder {
     }
 
     public UserUpdateRequestTestBuilder 프로필_이미지_URL이(String 프로필_이미지_URL) {
-        this.imageUrl = 프로필_이미지_URL;
-        return this;
-    }
-
-    public UserUpdateRequestTestBuilder 링크가(List<String> 링크) {
-        this.links = 링크;
+        this.profileImageUrl = 프로필_이미지_URL;
         return this;
     }
 
     public UserUpdateRequest 이다() {
         return UserUpdateRequest.builder()
-                .nickname(nickname)
-                .bio(bio)
-                .imageUrl(imageUrl)
-                .links(links)
+                .nickname(JsonNullable.of(nickname))
+                .bio(JsonNullable.of(bio))
+                .profileImageUrl(JsonNullable.of(profileImageUrl))
                 .build();
     }
 }
