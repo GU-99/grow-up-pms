027a33a434e78c0ca7716605a7cb8817f8a8312d
wlshooo <kswlsgh@naver.com>
2024-09-05 23:44:36 +0900

    Refactor: 링크 테스트에서 사용하기 위한 Builder 추가

--- src/main/java/com/growup/pms/user/controller/dto/request/UserLinksUpdateRequest.java
+++ src/main/java/com/growup/pms/user/controller/dto/request/UserLinksUpdateRequest.java
@@ -3,6 +3,7 @@ package com.growup.pms.user.controller.dto.request;
 import com.growup.pms.user.service.dto.UserLinksUpdateCommand;
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
@@ -11,6 +12,11 @@ import org.openapitools.jackson.nullable.JsonNullable;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserLinksUpdateRequest {
     private JsonNullable<List<@URL @Length(max = 255) String>> links = JsonNullable.undefined();
+
+    @Builder
+    public UserLinksUpdateRequest(JsonNullable<List<@URL @Length(max = 255) String>> links) {
+        this.links = links;
+    }
 
     public UserLinksUpdateCommand toCommand() {
         return UserLinksUpdateCommand.builder()
