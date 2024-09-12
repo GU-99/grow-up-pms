b9241971ead4dc79c2251e1ec2beea3ea00c7cd6
yachimiya <hachimiya@pm.me>
2024-09-12 16:38:46 +0900

    Feat: S3에 파일을 업로드 및 다운로드하는 기능을 구현함

---
+++ src/main/java/com/growup/pms/common/config/S3Config.java
@@ -0,0 +1,35 @@
+package com.growup.pms.common.config;
+
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .overrideConfiguration(
+                        builder -> builder.apiCallTimeout(Duration.ofSeconds(90L))
+                                .apiCallAttemptTimeout(Duration.ofSeconds(20L)))
+                .build();
+    }
+}
