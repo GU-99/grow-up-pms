5f949f7824b7c0083ff690a60a9487849120ae50
yachimiya <8318212+yachimiya@users.noreply.github.com>
2024-09-13 18:44:47 +0900

    Feat: #231 현재 사용자 정보 조회 API의 응답에서 사용자 ID, 프로바이더를 같이 내려주도록 수정함 (#232)
    * Feat: 현재 사용자 정보 조회 API의 응답에서 사용자 ID, 프로바이더를 같이 내려주도록 수정함
    
    * Docs: 누락된 API 명세 파일

--- src/test/java/com/growup/pms/test/fixture/user/builder/UserResponseTestBuilder.java
+++ src/test/java/com/growup/pms/test/fixture/user/builder/UserResponseTestBuilder.java
@@ -1,6 +1,7 @@
 package com.growup.pms.test.fixture.user.builder;
 
 import com.growup.pms.user.controller.dto.response.UserResponse;
+import com.growup.pms.user.domain.Provider;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserResponseTestBuilder {
+    private Long userId = 1L;
     private String username = "brown";
     private String email = "brown@growup.kr";
+    private Provider provider = Provider.LOCAL;
     private String nickname = "브라운";
     private String bio = "안녕하세요, 브라운입니다!";
     private String profileImageName = "728f3af8-4080-45b1-8b3c-d25e5e073dc7.png";
@@ -21,6 +24,11 @@ public class UserResponseTestBuilder {
         return new UserResponseTestBuilder();
     }
 
+    public UserResponseTestBuilder 식별자가(Long 식별자) {
+        this.userId = 식별자;
+        return this;
+    }
+
     public UserResponseTestBuilder 아이디가(String 아이디) {
         this.username = 아이디;
         return this;
@@ -28,6 +36,11 @@ public class UserResponseTestBuilder {
 
     public UserResponseTestBuilder 이메일이(String 이메일) {
         this.email = 이메일;
+        return this;
+    }
+
+    public UserResponseTestBuilder 프로바이더가(Provider 프로바이더) {
+        this.provider = 프로바이더;
         return this;
     }
 
@@ -53,8 +66,10 @@ public class UserResponseTestBuilder {
 
     public UserResponse 이다() {
         return UserResponse.builder()
+                .userId(userId)
                 .username(username)
                 .email(email)
+                .provider(provider)
                 .nickname(nickname)
                 .bio(bio)
                 .profileImageName(profileImageName)
