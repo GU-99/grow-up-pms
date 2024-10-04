9ff629f8f8d75eb28901653ad6b5777a9afb00f9
longBright <74171272+longBright@users.noreply.github.com>
2024-10-04 17:42:30 +0900

    Feat: #254 프로젝트 일정 수행자 제거 기능 구현 (#277)
    * Feat: 일정 수행자 ID로 조회하는 쿼리 구현
    
    * Feat: 일정 수행자 제거 기능 구현
    
    * Test: 일정 수행자 제거 기능 테스트
    
    * Test: 일정 수행자 제거 API 문서 작성

--- src/main/java/com/growup/pms/task/repository/TaskUserRepository.java
+++ src/main/java/com/growup/pms/task/repository/TaskUserRepository.java
@@ -1,5 +1,7 @@
 package com.growup.pms.task.repository;
 
+import com.growup.pms.common.exception.code.ErrorCode;
+import com.growup.pms.common.exception.exceptions.BusinessException;
 import com.growup.pms.task.domain.TaskUser;
 import com.growup.pms.task.domain.TaskUserId;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TaskUserRepository extends JpaRepository<TaskUser, TaskUserId>, TaskUserQueryRepository {
 
+    default TaskUser findByIdOrThrow(TaskUserId id) {
+        return findById(id).orElseThrow(() -> new BusinessException(ErrorCode.PROJECT_USER_NOT_FOUND));
+    }
 }
