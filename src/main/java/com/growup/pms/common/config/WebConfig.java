079e1c60ee3724153e9ec3a35736f8bdfcb61874
yachimiya <hachimiya@pm.me>
2024-11-04 12:32:36 +0900

    Config: 크로스 도메인 문제를 해결하기 위해 쿠키에 SameSite에서 None을 전달함

---
+++ src/main/java/com/growup/pms/common/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.growup.pms.common.config;
+
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public TomcatContextCustomizer sameSiteCookiesConfig() {
+        return context -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        };
+    }
+}
