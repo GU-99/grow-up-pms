03373a8a1c715c28a5ee40eaa968750e1aa81b2a
yachimiya <hachimiya@pm.me>
2024-11-06 14:57:20 +0900

    Feat: Secure 속성을 활성화함

--- src/main/java/com/growup/pms/common/util/CookieUtil.java
+++ src/main/java/com/growup/pms/common/util/CookieUtil.java
@@ -11,8 +11,7 @@ public final class CookieUtil {
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setHttpOnly(true);
-        // TODO: 서비스가 HTTPS로 배포된 후에 보안 강화를 위해 주석을 해제해야 함
-        // refreshTokenCookie.setSecure(true);
+        cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setMaxAge(maxAge);
         response.addCookie(cookie);
@@ -21,6 +20,7 @@ public final class CookieUtil {
     public static void removeCookie(HttpServletResponse response, String name) {
         Cookie cookie = new Cookie(name, null);
         cookie.setHttpOnly(true);
+        cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setMaxAge(0);
         response.addCookie(cookie);
