2bb64516e6d9e4557ab0a52b68f67eae6710283f
longBright <longbright0804@gmail.com>
2024-12-04 15:40:16 +0900

    Chore: jackson 직렬화 관련 설정 추가

--- src/main/java/com/growup/pms/common/config/ObjectMapperConfig.java
+++ src/main/java/com/growup/pms/common/config/ObjectMapperConfig.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -27,8 +29,9 @@ public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper(ObjectMapper objectMapper) {
-        objectMapper.findAndRegisterModules();
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .setDateFormat(new StdDateFormat().withColonInTimeZone(true));
         return objectMapper;
     }
 
