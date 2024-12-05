104dfa607793cc1733e0778a307863447eb1158f
longBright <longbright0804@gmail.com>
2024-12-06 08:20:20 +0900

    Chore: jackson 직렬화 관련 설정 변경

--- src/main/java/com/growup/pms/common/config/ObjectMapperConfig.java
+++ src/main/java/com/growup/pms/common/config/ObjectMapperConfig.java
@@ -2,9 +2,7 @@ package com.growup.pms.common.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -23,20 +21,17 @@ public class ObjectMapperConfig {
             builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
             builder.featuresToDisable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
             builder.featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-            builder.modules(jsonNullableModule());
+            builder.modules(jsonNullableModule(), javaTimeModule());
         };
-    }
-
-    @Bean
-    public ObjectMapper objectMapper(ObjectMapper objectMapper) {
-        objectMapper.registerModule(new JavaTimeModule())
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-                .setDateFormat(new StdDateFormat().withColonInTimeZone(true));
-        return objectMapper;
     }
 
     @Bean
     public JsonNullableModule jsonNullableModule() {
         return new JsonNullableModule();
+    }
+
+    @Bean
+    public JavaTimeModule javaTimeModule() {
+        return new JavaTimeModule();
     }
 }
