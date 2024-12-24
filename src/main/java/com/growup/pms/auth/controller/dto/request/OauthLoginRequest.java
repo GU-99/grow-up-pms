cae680fcd76893f4cede0df004bfbd4eb149f00e
wlshooo <kswlsgh@naver.com>
2024-12-24 11:04:09 +0900

    Refactor: 컨벤션에 맞게 들여쓰기 수정

--- src/main/java/com/growup/pms/auth/controller/dto/request/OauthLoginRequest.java
+++ src/main/java/com/growup/pms/auth/controller/dto/request/OauthLoginRequest.java
@@ -4,8 +4,8 @@ import com.growup.pms.auth.service.dto.oauth.OauthUserLoginCommand;
 import lombok.Builder;
 
 @Builder
-public record OauthLoginRequest(String code)
-{
+public record OauthLoginRequest(String code) {
+
     public OauthUserLoginCommand toCommand() {
         return  OauthUserLoginCommand.builder()
                 .code(code)
