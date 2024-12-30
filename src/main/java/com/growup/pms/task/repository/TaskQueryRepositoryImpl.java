abc024afb116167301739054d8c8b8f66dd20e94
longbright <longbright0804@gmail.com>
2024-12-30 22:43:52 +0900

    Feat: 상태별 일정 리스트 조회 쿼리 구현 & 기존 쿼리 삭제

--- src/main/java/com/growup/pms/task/repository/TaskQueryRepositoryImpl.java
+++ src/main/java/com/growup/pms/task/repository/TaskQueryRepositoryImpl.java
@@ -1,19 +1,14 @@
 package com.growup.pms.task.repository;
 
-import static com.growup.pms.project.domain.QProject.project;
 import static com.growup.pms.status.domain.QStatus.status;
 import static com.growup.pms.task.domain.QTask.task;
-import static java.util.stream.Collectors.groupingBy;
 
 import com.growup.pms.task.controller.dto.response.TaskResponse;
-import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
@@ -25,53 +20,43 @@ public class TaskQueryRepositoryImpl implements TaskQueryRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    @Override
-    public Map<Long, List<TaskResponse>> getTasksByProjectId(Long projectId) {
-        List<Long> ids = queryFactory
-                .select(task.id)
+    public void decreaseSortOrderByStatus(Long statusId, Short sortOrder) {
+        queryFactory.update(task)
+                .set(task.sortOrder, task.sortOrder.subtract(1))
+                .where(isStatusId(statusId), task.sortOrder.gt(sortOrder))
+                .execute();
+    }
+
+    public List<TaskResponse> getAllTasksByStatus(Long statusId) {
+        List<Long> ids = queryFactory.select(task.id)
                 .from(task)
                 .join(task.status, status)
-                .join(task.status.project, project)
                 .where(
-                        isProjectId(projectId)
+                        isStatusId(statusId)
                 )
                 .orderBy(task.sortOrder.asc())
                 .fetch();
 
         if (ids.isEmpty()) {
-            return Collections.emptyMap();
+            return Collections.emptyList();
         }
 
-        List<Tuple> results = queryFactory
-                .select(task.status.id,
-                        Projections.constructor(TaskResponse.class,
-                                task.id,
-                                task.status.id,
-                                task.name,
-                                task.content,
-                                task.sortOrder,
-                                task.startDate,
-                                task.endDate
-                        ))
+        return queryFactory.select(Projections.constructor(TaskResponse.class,
+                        task.id,
+                        task.status.id,
+                        task.name,
+                        task.content,
+                        task.sortOrder,
+                        task.startDate,
+                        task.endDate
+                ))
                 .from(task)
                 .join(task.status, status)
-                .join(task.status.project, project)
                 .where(
                         task.id.in(ids)
                 )
                 .orderBy(task.sortOrder.asc())
                 .fetch();
-
-        return results.stream()
-                .map(tuple -> tuple.get(1, TaskResponse.class))
-                .collect(groupingBy(task -> Objects.requireNonNull(task).getStatusId()));
-    }
-
-    public void updateSortOrderInStatus(Long statusId, Short sortOrder) {
-        queryFactory.update(task)
-                .set(task.sortOrder, task.sortOrder.subtract(1))
-                .where(isStatusId(statusId), task.sortOrder.gt(sortOrder))
-                .execute();
     }
 
     private BooleanExpression isProjectId(Long projectId) {
